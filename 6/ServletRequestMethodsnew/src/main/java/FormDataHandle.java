e4a55de4e85b940cfa1422f4c3eef4c2fc01fae7
Kishore Karra <kishore.0331@gmail.com>
2025-08-30 02:13:33 +0530

    Add files via upload

---
+++ 6/ServletRequestMethodsnew/src/main/java/FormDataHandle.java
@@ -0,0 +1,68 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+  
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+  
+// Servlet implementation class FormDataHandle
+  
+// Annotation to map the Servlet URL
+@WebServlet("/FormData")
+public class FormDataHandle extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	  
+    // Auto-generated constructor stub
+    public FormDataHandle() {
+        super();
+    }
+  
+    // HttpServlet doPost(HttpServletRequest request, HttpServletResponse response) method
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+          
+        // Get the values from the request using 'getParameter'
+        String name = request.getParameter("name");
+        String phNum = request.getParameter("phone");
+        String gender = request.getParameter("gender");
+          
+        // To get all the values selected for 
+        // programming language, use 'getParameterValues'
+        String progLang[] = request.getParameterValues("language");
+        
+        // Iterate through the String array to 
+        // store the selected values in form of String
+        String langSelect = "";
+        if(progLang!=null){
+            for(int i=0;i<progLang.length;i++){
+                langSelect= langSelect + progLang[i]+ ", ";
+            }
+        }
+          
+        String courseDur = request.getParameter("duration");
+        String comment = request.getParameter("comment");
+                  
+        // set the content type of response to 'text/html'
+        response.setContentType("text/html");
+          
+        // Get the PrintWriter object to write 
+        // the response to the text-output stream
+        PrintWriter out = response.getWriter();
+          
+        // Print the data
+        out.print("<html><body>");
+        out.print("<h3>Details Entered</h3><br/>");
+          
+        out.print("Full Name:<b> "+ name + "</b><br/>");
+        out.print("Phone Number:<b> "+ phNum +"</b><br/>");
+        out.print("Gender:<b> "+ gender +"</b><br/>");
+        out.print("Programming languages selected:<b> "+ langSelect +"</b><br/>");
+        out.print("Duration of course:<b> "+ courseDur+"</b><br/>");
+        out.print("Comments:<b> "+ comment+"</b>");
+          
+        out.print("</body></html>");
+          
+    }
+  
+}
